7c09a68531caa44943fa9dda09e5079a525fa1f4
Bianca <alvesbianca9118@gmail.com>
2026-03-18 20:35:07 -0300

    Modificando a classe Principal.java

--- conta-banco/src/Principal.java
+++ conta-banco/src/Principal.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
 public class Principal {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
         Scanner entrada = new Scanner(System.in);
         System.out.println("Informe o número da sua conta bancária:");
         int numeroConta = entrada.nextInt();
